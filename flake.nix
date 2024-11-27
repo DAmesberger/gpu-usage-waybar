c6c1d17db6e48f9c0d57348eb9c5a4c2fbff18c9
Daniel <daniel.amesberger@amescon.com>
2024-11-27 18:01:53 +0100

    changed back to default attribute

--- flake.nix
+++ flake.nix
@@ -4,33 +4,26 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
   };
   outputs = { self, nixpkgs }: {
-    packages = {
-      x86_64-linux = let
-        pkgs = import nixpkgs {
-          system = "x86_64-linux";
-        };
-        rustPlatform = pkgs.rustPlatform;
-      in rustPlatform.buildRustPackage {
-        pname = "gpu-usage-waybar";
-        version = "0.1.12";
-        src = ./.;
-        cargoHash = "sha256-q8fxOetIhh1vZi/KVO+yur99yfzycizYUSdqC7cV48I=";
-        meta = with pkgs.lib; {
-          description = "Add GPU usage to Waybar";
-          license = licenses.mit;
-          maintainers = [ "Daniel Amesberger" ];
-        };
-        nativeBuildInputs = [ pkgs.makeWrapper ];
-        postFixup = ''
-          wrapProgram $out/bin/gpu-usage-waybar \
-            --prefix LD_LIBRARY_PATH : /run/opengl-driver/lib
-        '';
+    packages.x86_64-linux.default = let  # Changed this line
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
       };
-    };
-    nixosModules.default = { config, ... }: {
-      config.environment.systemPackages = [
-        self.packages.${config.system}
-      ];
+      rustPlatform = pkgs.rustPlatform;
+    in rustPlatform.buildRustPackage {
+      pname = "gpu-usage-waybar";
+      version = "0.1.12";
+      src = ./.;
+      cargoHash = "sha256-q8fxOetIhh1vZi/KVO+yur99yfzycizYUSdqC7cV48I=";
+      meta = with pkgs.lib; {
+        description = "Add GPU usage to Waybar";
+        license = licenses.mit;
+        maintainers = [ "Daniel Amesberger" ];
+      };
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+      postFixup = ''
+        wrapProgram $out/bin/gpu-usage-waybar \
+          --prefix LD_LIBRARY_PATH : /run/opengl-driver/lib
+      '';
     };
   };
 }
